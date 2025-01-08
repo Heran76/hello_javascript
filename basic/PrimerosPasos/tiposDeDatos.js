b9a28e0dc7aecc3bca71e5f6f8811fec2afdae6c
Heran76 <heran76@gmail.com>
2025-01-08 14:47:54 +0100

    Undifened

--- basic/PrimerosPasos/tiposDeDatos.js
+++ basic/PrimerosPasos/tiposDeDatos.js
@@ -3,10 +3,19 @@
 // Tipos de datos primitivos
 
 // Cadenas de texto (string)
-let name = "Brais Moure"
+let myName = "Brais Moure"
 let alias = 'MoureDev'
 let email  = `braismoure@mouredev.com`
 
 // Números
 let age = 37 // Entero
 let height = 1.77 // Decimal
+
+// Booleanos (booleano)
+let  isTeacher = true
+let isStudent = false
+// Undefined
+let undefinedValue
+console.log(undefinedValue)
+
+console.log(typeof(myName))
