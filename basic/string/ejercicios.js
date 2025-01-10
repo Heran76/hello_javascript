181d3429ffd95d169ca8a5048924d56b1674ed5f
Heran76 <heran76@gmail.com>
2025-01-10 17:09:29 +0100

    Ejercicio-2

--- basic/string/ejercicios.js
+++ basic/string/ejercicios.js
@@ -3,4 +3,11 @@
 let string1 = "Hello";
 let string2 = "World";
 let concatenated = string1 + " " + string2;
+console.log("*** Ejercicio 1 ****")
 console.log(concatenated)
+
+// 2. Muestra la longitud de una cadena de texto.
+
+let mystring = "Hello World"
+console.log("*** Ejercicio 2 ****")
+console.log(mystring.length)
