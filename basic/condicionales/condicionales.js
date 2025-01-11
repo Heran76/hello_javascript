39c45518d5d69d1d686e3b5086ad8750f8c2c47c
Heran76 <heran76@gmail.com>
2025-01-11 11:32:41 +0100

    Condicionales

---
+++ basic/condicionales/condicionales.js
@@ -0,0 +1,13 @@
+//condicionales
+/*
+Los condicionales permiten ejecutar difirentes bloques de código
+en función de si se cumplen o no determinadas condiciones, Este control
+de flujo es clave para crear programas que puedan responder de manera dinámica
+a diferente soluciones.
+if.
+else
+else if
+operador ternario
+switch
+*/
+
