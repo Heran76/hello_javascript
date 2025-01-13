ee18ee6315dfcac7fd838701f05023ba9965147a
Heran76 <heran76@gmail.com>
2025-01-13 18:12:29 +0100

    Estrucutras-arrays

---
+++ basic/estructuras/arrays.js
@@ -0,0 +1,9 @@
+//Arrays
+/*
+Vamos a explorar el uso de arrays en Javascript, una extructura de datos
+fundamental que permite almacenar multiplies valores en una sola variable
+los arrays son muy flexibles y se utilizan para manejar listas de datos 
+como números, cadenas de texto o incluso objetos, los arrays te permiten
+agregar, eliminar, acceder y modificar elementos fácilmente, con ellos
+podrás organizar y manipular colecciones de datos de manera eficiente.
+*/
