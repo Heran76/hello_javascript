c63c1cf3e9396fb74e6d5237e721aa9d1feed5b6
Heran76 <heran76@gmail.com>
2025-01-23 13:51:53 +0100

    Funciones

---
+++ basic/funciones/funciones.js
@@ -0,0 +1,13 @@
+// funciones
+/*
+Las funciones nos permiten agrupar bloques de código reutilizables, aquellos
+que nos ayudan a realizar tareas especificas de manera eficiente y organizada. 
+Así podremos escribir código modular, maás limpio y fácil de mantener
+*/
+
+function myfun(){
+    console.log("Hello fuction")
+}
+for(let i = 0; i<5; i++){
+    myfun();
+}
