411f59617aa3e03ef1eec1fe0248910a13ab0c47
Heran76 <heran76@gmail.com>
2025-01-25 21:44:10 +0100

    Ejercicio 2

--- basic/funciones/ejercicios.js
+++ basic/funciones/ejercicios.js
@@ -5,3 +5,23 @@
 function sum(a, b) {
     return a + b
 }
+
+// 2. Crea una función que reciba un array de números y devuelva el mayor de ellos.
+console.log("***** Ejercicio 2 *****")
+function findMax(numbers) {
+    let max = numbers[0]
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i]
+        }
+    }
+    return max
+}
+// Definimos el array de prueba
+const numbers = [3, 7, -2, 10, 5, 1, 8];
+
+// Llamamos a la función y almacenamos el resultado
+const maxNumber = findMax(numbers);
+
+// Mostramos el resultado en la consola
+console.log(`El mayor número del array es: ${maxNumber}`);
