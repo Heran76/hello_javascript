ccada7c17f5987bd52c392bcaf76dcb9325da7a7
Heran76 <heran76@gmail.com>
2025-01-25 20:10:56 +0100

    Ejercicios - Funciones

---
+++ basic/funciones/ejercicios.js
@@ -0,0 +1,7 @@
+// NOTA: Explora diferentes sintaxis de funciones para resolver los ejercicios.
+
+// 1. Crea una función que reciba dos números y devuelva su suma.
+
+function sum(a, b) {
+    return a + b
+}
