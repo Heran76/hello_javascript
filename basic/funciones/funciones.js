77c5b4c9b5ae8c192a1c6c5ac552ab83e5b84ce0
Heran76 <heran76@gmail.com>
2025-01-25 19:20:41 +0100

    funciones

--- basic/funciones/funciones.js
+++ basic/funciones/funciones.js
@@ -41,10 +41,66 @@ function sum(a,b){
 sum(40,2)
 sum(42) //error NAN
 sum() //error NAN
+const myFunc4 = (name) => console.log(`Hola, ${name}!`)
+myFunc4("Antonio Heredia")
 
 function defaultsum(a=0,b=0){
     console.log(a+b);
 }
-defaultsum()
-defaultsum(76)
-defaultsum(70,6)
+// por defecto
+defaultsum();
+defaultsum(76);
+defaultsum(70,6);
+defaultsum(b=5);
+
+// Retorno de valores
+
+function mult(a, b) {
+    return a * b
+}
+
+let result = mult(5, 10)
+console.log(result)
+
+// Funciones anidadas
+
+function extern() {
+    console.log("Función externa")
+    function intern() {
+        console.log("Función interna")
+    }
+    intern()
+}
+
+extern()
+// intern() Error: fuera del scope
+
+// Funciones de orden superior
+
+function applyFunc(func, param) {
+    func(param)
+}
+
+applyFunc(myFunc4, "función de orden superior")
+
+// forEach
+
+let myArray = [1, 2, 3, 4]
+
+let mySet = new Set(["Antonio", "Heredia", "Heran76", 76, true, "Heran76@gmail.com"])
+
+let myMap = new Map([
+    ["name", "Antonio"],
+    ["email", "Antonio heredia"],
+    ["age", 76]
+])
+
+myArray.forEach(function (value) {
+    console.log(value)
+})
+
+myArray.forEach((value) => console.log(value))
+
+mySet.forEach((value) => console.log(value))
+
+myMap.forEach((value) => console.log(value))
