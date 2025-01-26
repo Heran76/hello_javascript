33a5468477c7a48fb2d30a012d3be391291e866b
Heran76 <heran76@gmail.com>
2025-01-26 11:45:42 +0100

    Ejercicio 4

--- basic/funciones/ejercicios.js
+++ basic/funciones/ejercicios.js
@@ -5,7 +5,8 @@
 function sum(a, b) {
     return a + b
 }
-
+console.log("***** Ejercicio 1 *******")
+console.log(sum(70,6))
 // 2. Crea una función que reciba un array de números y devuelva el mayor de ellos.
 console.log("***** Ejercicio 2 *****")
 function findMax(numbers) {
@@ -25,3 +26,35 @@ const maxNumber = findMax(numbers);
 
 // Mostramos el resultado en la consola
 console.log(`El mayor número del array es: ${maxNumber}`);
+
+// 3. Crea una función que reciba un string y devuelva el número de vocales que contiene.
+function countVowels(str) {
+    let vowels = "aeiouáéíóú"
+    let count = 0
+    let lowerCaseStr = str.toLowerCase()
+    for (let char of lowerCaseStr) {
+        if (vowels.includes(char)) {
+            count++
+        }
+    }
+    return count
+}
+console.log("***** Ejercicio 3 *****")
+console.log("Hola ¿Como estas? tiene "+ countVowels("Hola, ¿cómo estás?")+" vocoles"); // 6
+console.log(countVowels("Programación"));      // 5
+console.log(countVowels("12345"));             // 0
+console.log(countVowels("HELLO"));             // 2
+
+
+
+// 4. Crea una función que reciba un array de strings y devuelva un nuevo array con el strings en mayúsculas.
+function toUpperCaserArray(strings){
+    let upperCaseStrings = []
+    for(let i = 0; i < strings.length; i++){
+        upperCaseStrings.push(strings[i].toUpperCase())
+        return upperCaseStrings
+    }
+}
+let strings = ["holacaracola"]
+console.log("***** Ejercicio 4 *****")
+console.log(toUpperCaserArray(strings))
