1d7c72124c884fe06aa2b316554810bdea3a57ac
Heran76 <heran76@gmail.com>
2025-02-02 11:29:27 +0100

    Anidación de objetos

--- basic/objetos/objetos.js
+++ basic/objetos/objetos.js
@@ -1,4 +1,12 @@
-//Objects -- objetos
+//Objects -- 
+/*
+Los objetos son extructuras de datos que permiten almacenar valores como
+pares clave : valor, lo que te permite representar entidades complejas y organizar
+información de manera más clara. En javascript, casi todo es un objeto o se puede
+tratar como un objeto. lo que convierte a este concepot en una pieza clave del lenguaje
+los objetos nos permiten modelar datos del mundo real, asi como contener tanto propiedad valor como metedos ( funciones)
+
+*/
 //sintaxis
 //objecto_literal
 let person = {
@@ -19,4 +27,43 @@ console.log(person.name)
 //eleminar propiedad con la palabra reservdada delete acontinuacion nombre obejto y nomenclatura del punto
 delete person.age
 console.log(person)
-//añadir con la nomenclatura del punto
+//añadir con la nomenclatura del 
+person.email = "heran76@gmail.com"
+person["age"] = 37 //aqui hemos utilizado corchetes para añadir
+console.log(person)
+
+//Métodos (funciones)
+let person2 = {
+    age : 49,
+    name: "Marcos",
+    walk : function(){
+        console.log(`${this.name}`)
+        console.log("La persona camina")
+    }
+}
+
+person2.walk();
+// Anidación de objetos
+
+let person3 = {
+    name: "Heran",
+    age: 48,
+    alias: "heranDev",
+    walk: function () {
+        console.log("La persona camina.")
+    },
+    job: {
+        name: "Programador",
+        exp: 3,
+        work: function () {
+            console.log(`La persona de ${this.exp} años de experiencia trabaja.`)
+        }
+    }
+}
+
+console.log(person3)
+
+console.log(person3.name)
+console.log(person3.job)
+console.log(person3.job.name)
+person3.job.work()
