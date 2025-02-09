ed2d5236bdc55f0e9611956fe589d422d2229984
Heran76 <heran76@gmail.com>
2025-02-09 19:20:54 +0100

    Desestruccion

---
+++ desestructuracion_propagacion/desestructuracion_progacion.js
@@ -0,0 +1,22 @@
+/*
+La desestructuración en JavaScript es una sintaxis que permite extraer valores de arrays o propiedades de objetos en variables individuales de manera más concisa.
+
+Ejemplos:
+Arrays:
+
+javascript
+Copy
+const numeros = [1, 2, 3];
+const [a, b, c] = numeros;
+console.log(a); // 1
+console.log(b); // 2
+Objetos:
+
+javascript
+Copy
+const persona = { nombre: 'Juan', edad: 30 };
+const { nombre, edad } = persona;
+console.log(nombre); // 'Juan'
+console.log(edad); // 30
+Es útil para simplificar el acceso a datos en estructuras complejas.
+*/
