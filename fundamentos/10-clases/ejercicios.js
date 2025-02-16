32710a4a1d550aea2fc5defc533a5773f460357e
Heran76 <heran76@gmail.com>
2025-02-16 20:43:17 +0100

    Ejercciio 2

--- fundamentos/10-clases/ejercicios.js
+++ fundamentos/10-clases/ejercicios.js
@@ -4,3 +4,16 @@ class Person {
         this.name = name
         this.age = age
     }
+
+// 2. AÃ±ade un método a la clase que utilice las propiedades.
+
+       greet() {
+        console.log(`Hola, mi nombre es ${this.name} y tengo ${this.age} aÃ±os`)
+    }
+
+    // 4. Añade un método estático a la primera clase.
+
+    static welcome() {
+        console.log("Hola, soy un método estático de esta clase")
+    }
+}
