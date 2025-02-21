a84cda92359b6479fd18bae1c0c893a08c4ac2ec
Heran76 <heran76@gmail.com>
2025-02-21 16:49:25 +0100

    manejo de errores

---
+++ fundamentos/11-manejo_errores/manejoErrores.js
@@ -0,0 +1,12 @@
+//Manejo de errores
+/*
+El manejo de errores es esencial para garantizar que tus aplicaciones puedan 
+manejar situaciones inesperadas y continuar ejecutandose sin interrupciones o,
+al menos, brindar información clara sobre que salíó mal.
+
+Javascript proporciona herramientas como try, catch, throw y finally para manejar
+expeciones de manera robusta. Además, es posible crear tus propias excepciones
+personalizadas utlizando la herencia de la clase Error, lo que permite definir tipos
+de errores especificos para diferentes situaciones en tu código.
+
+*/
