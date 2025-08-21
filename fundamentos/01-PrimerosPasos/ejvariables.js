cebaa6e05d3a1d6cf88a5fcd998f60504b5f94b0
Heran76 <heran76@gmail.com>
2025-08-21 17:30:25 +0200

    ejvariables

---
+++ fundamentos/01-PrimerosPasos/ejvariables.js
@@ -0,0 +1,20 @@
+var miNombre = "Antonio";
+let number = 2 // np es un buen uso 
+function nombre(){
+    let miApellido = "Heredia"; // aqui si es un buen uso
+    let number = 49
+    console.log(miNombre +" "+miApellido + " "+number);
+}
+
+nombre()
+//console.log(mi);
+console.log(miNombre);
+console.log(number);
+
+const person ={
+    name : "Antonio Heredia"
+}
+
+console.log(person.name);
+person.name = "Antonio Morante"
+console.log(person.name);
